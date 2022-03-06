bb5e8a6fa698264060eab750ebc69861d4eb6165
CHUSGF <chusgf2906@gmail.com>
2022-03-06 08:11:41 +0000

    cambio de maquetacion y web responsive

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,19 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import TodoList from "./todolist.jsx";
 
 //create your first component
 const Home = () => {
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container mt-5 todo mx-auto">
+			<div className="row">
+				<p className="title display-2 text-center mt-2 fst-italic">
+					Todo list
+				</p>
+				<div className="col-12">
+					<TodoList />
+				</div>
+			</div>
 		</div>
 	);
 };
-
 export default Home;
